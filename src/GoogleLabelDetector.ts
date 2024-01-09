ad1f5202ed71ea683090d3dd2d95c3d2869ca858
noah-bar <noah.barberini@eduvaud.ch>
2024-01-09 19:59:36 +0100

    feat: Implement analyse method

--- src/GoogleLabelDetector.ts
+++ src/GoogleLabelDetector.ts
@@ -1,5 +1,6 @@
 import { ILabelDetector, Label } from "ILabelDetector";
 import vision, {ImageAnnotatorClient} from "@google-cloud/vision";
+import { existsSync } from 'fs'
 
 export default class GoogleLabelDetector implements ILabelDetector {
     protected client: ImageAnnotatorClient
@@ -7,7 +8,19 @@ export default class GoogleLabelDetector implements ILabelDetector {
         this.client = new vision.ImageAnnotatorClient({keyFilename})
     }
     
-    analyze(imagePath: string, maxLabels?: number | undefined, minConfidenceLevel?: number | undefined): Promise<Label[]> {
-        throw new Error("Method not implemented.");
+    public async analyze(imagePath: string, maxResults: number = 7, minConfidenceLevel: number = 90): Promise<Label[]> {
+        let image: { source: { filename: string } | { imageUri: string } } = { source: { filename: imagePath } }
+        if(!existsSync(imagePath)) {
+            image = { source: { imageUri: imagePath } }
+        }
+
+        const [result] = await this.client.annotateImage({
+            image: image,
+            features: [{type: 'LABEL_DETECTION', maxResults: maxResults}]
+        });
+        if(!result.labelAnnotations) return []
+        return result.labelAnnotations
+            .map(label => ({ description: label.description, score: (label.score as number * 100) }) as Label)
+            .filter(label => label.score >= minConfidenceLevel)
     }
 }
