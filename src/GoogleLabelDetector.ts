cbd6122c3190fc15059bba3b004dd440514f76ef
noah-bar <noah.barberini@eduvaud.ch>
2024-01-09 19:59:06 +0100

    feat: Add GoogleLabelDetector constructor

--- src/GoogleLabelDetector.ts
+++ src/GoogleLabelDetector.ts
@@ -1,6 +1,12 @@
 import { ILabelDetector, Label } from "ILabelDetector";
+import vision, {ImageAnnotatorClient} from "@google-cloud/vision";
 
 export default class GoogleLabelDetector implements ILabelDetector {
+    protected client: ImageAnnotatorClient
+    constructor(keyFilename: string) {
+        this.client = new vision.ImageAnnotatorClient({keyFilename})
+    }
+    
     analyze(imagePath: string, maxLabels?: number | undefined, minConfidenceLevel?: number | undefined): Promise<Label[]> {
         throw new Error("Method not implemented.");
     }
