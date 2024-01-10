a9303c8ae423f6b79406c50360b2325d1352568c
noah-bar <noah.barberini@eduvaud.ch>
2024-01-10 15:29:24 +0100

    feat: Add express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import GoogleLabelDetector from "./GoogleLabelDetector";
-import { ILabelDetector, Label } from "./ILabelDetector";
+import express, {Request, Response, Router} from 'express'
+import multer from 'multer'
+import { resolve } from "path";
 
-export {
-  GoogleLabelDetector,
-  ILabelDetector,
-  Label
-}
+const PORT = 3000
+const app = express()
+const router = Router()
+const upload = multer()
+
+const keyFilename = resolve("./config/key.json")
+const googleLabelDetector = new GoogleLabelDetector(keyFilename)
+
+app.use(express.json())
+
+app.use("/api/v1", router)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
