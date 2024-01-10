e933ffa0005922ca2595df074a92b658c8e4cf32
noah-bar <noah.barberini@eduvaud.ch>
2024-01-10 15:29:59 +0100

    feat: add /api/v1/analyse endpoint to return image labels

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,19 @@ const googleLabelDetector = new GoogleLabelDetector(keyFilename)
 
 app.use(express.json())
 
+router.post('/analyse',upload.single('image'), async (req: Request, res: Response) => {
+  const image: string | undefined = req.body.image
+  if(!image) {
+    return res.status(401).send({
+      errors: ["The request must contain an image"]
+    })
+  }
+  const maxResult = req.body.maxResult || 7
+  const minConfidenceLevel = req.body.maxResult || 90
+  const labels = await googleLabelDetector.analyze(image, maxResult, minConfidenceLevel)
+  res.json(labels)
+})
+
 app.use("/api/v1", router)
 
 app.listen(PORT, () => {
